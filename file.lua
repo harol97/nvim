3fadfb432e4bdc6ea1c60c1eff2f8e5e96b079a9
ht <49042862+harol97@users.noreply.github.com>
2024-07-06 17:04:33 -0500

    Update file.lua

--- file.lua
+++ file.lua
@@ -38,3 +38,37 @@ require("telescope").setup({
     },
   },
 })
+
+
+-- nvim-tre
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+    side = "right"
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open=true
+    }
+  }
+})
